b2fbcdae622020a7013337d4abd2fb27370a90b5
paulon <paper.paulon@gmail.com>
2014-02-21 15:33:16 -0800

    update of api

--- usersnap-api.php
+++ usersnap-api.php
@@ -10,15 +10,16 @@
 		//All data will be sent with a multipart form upload
 		$reportid = $_POST['reportid'];
 		
-		$path = "./usersnap/".$reportid;
+		$path = "./usersnap/".$time();
 		//create new directory
         mkdir($path, 0777, true);
 		  
-		$req_dump = print_r($_POST, TRUE);
+		//$req_dump = print_r($_POST, TRUE);
+		$req_dump = var_export($_POST, TRUE);		
 		$fp = fopen($path."/information.txt", 'w');
 		fwrite($fp, $req_dump);
 		fclose($fp);
-		
+		$filename = $_FILES['file']['name'];
 		//copy screenshot to the directory
 		move_uploaded_file(
 			$_FILES['file']['tmp_name'], 
@@ -30,14 +31,24 @@
 		$additional_info = json_decode($additional_info);		
 		
 		//build query connection
-		$mysqli = new mysqli("localhost", "user", "password", "database");
-		$qry = "insert into ticket_screenshot(ticket_id,token,sendercomment,referer,reportid,browser,ipaddress,senderemail,subject) values
+		$mysqli = new mysqli("localhost", "psmadmin", "eBVRupvN", "psmhelp_admin");
+		$qry = "insert into ticket_screenshot(ticket_id,token,sendercomment,referer,reportid,browser,ipaddress,senderemail,subject,path) values
 			(".$additional_info->_ticketId.",'".mysqli_real_escape_string($mysqli,$_POST['token'])."','".mysqli_real_escape_string($mysqli,$_POST['sendercomment'])."',
 			'".mysqli_real_escape_string($mysqli,$_POST['referer'])."','".mysqli_real_escape_string($mysqli,$_POST['reportid'])."','".mysqli_real_escape_string($mysqli,$_POST['browser'])."','".mysqli_real_escape_string($mysqli,$_POST['ipaddress'])."',
-			'".mysqli_real_escape_string($myslqi,$additional_info->_email)."','".mysqli_real_escape_string($mysqli,$_POST['subject'])."')";							
+			'".mysqli_real_escape_string($myslqi,$additional_info->_email)."','".mysqli_real_escape_string($mysqli,$_POST['subject'])."','".$path."/".$filename."')";							
+		
+		$fp = fopen($path."/insert.txt", 'w');
+		fwrite($fp, $qry);
+		fclose($fp);
+		
 		$mysqli->query($qry);
 		
-		$qry = "update tickets set ticket_screenshot_id = " . $mysqli->lastinsertid . " where ticket_id=" . $aditional_info->_ticketId .";";
+		$qry = "update tickets set ticket_screenshot_id = " . $mysqli->insert_id . " where ticket_id=" . $aditional_info->_ticketId .";";
+			
+		$fp = fopen($path."/update.txt", 'w');
+		fwrite($fp, $qry);
+		fclose($fp);
+		
 		$mysqli->query($qry);
 		
 		echo "success";
